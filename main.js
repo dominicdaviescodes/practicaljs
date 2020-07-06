60406a1dfcd7cf283e45fe248b605dc6443c9331
dominicdaviescodes <codewithdom@gmail.com>
2020-07-06 23:12:02 +0100

    v1 add new todos

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
+// Version 1 - Arrays
+// store todos
 var todos = ['item 1', 'item 2', 'item 3'];
+// display todos
+console.log('My Todos: ', todos);
+// add todos use push, adds to end
+todos.push('item 4');
 console.log('My Todos: ', todos);
