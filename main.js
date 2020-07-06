fbbcbea39caa43cabc747cb5a89a94ff107f4cf2
dominicdaviescodes <codewithdom@gmail.com>
2020-07-06 23:24:39 +0100

    v1 update or edit todos

--- main.js
+++ main.js
@@ -2,7 +2,15 @@
 // store todos
 var todos = ['item 1', 'item 2', 'item 3'];
 // display todos
-console.log('My Todos: ', todos);
+// console.log('My Todos: ', todos);
 // add todos use push, adds to end
 todos.push('item 4');
+// console.log('My Todos: ', todos);
+
+// change a todo.
+// get the item: tell pc which item you want to change in bracket notation.
+// update it = "new value"
+todos[0] = 'old item 1';
 console.log('My Todos: ', todos);
+
+// delete todos
