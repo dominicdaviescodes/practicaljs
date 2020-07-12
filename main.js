82b7c4b72dc0a74ff490b21b935f4676d92fc676
dominicdaviescodes <codewithdom@gmail.com>
2020-07-12 14:26:26 +0100

    comment out code in js file from arrays and functions lessons

--- main.js
+++ main.js
@@ -1,19 +1,53 @@
 // Version 1 - Arrays
 // store todos
-var todos = ['item 1', 'item 2', 'item 3'];
+// var todos = ['item 1', 'item 2', 'item 3'];
 // display todos
 // console.log('My Todos: ', todos);
 // add todos use push, adds to end
-todos.push('item 4');
+// todos.push('item 4');
 // console.log('My Todos: ', todos);
 
 // change a todo.
 // get the item: tell pc which item you want to change in bracket notation.
 // update it = "new value"
-todos[0] = 'old item 1';
-console.log('My Todos: ', todos);
+// todos[0] = 'old item 1';
+// console.log('My Todos: ', todos);
 
 // delete todos
 // use splice()  tell where to start (position) and how many to delete
-todos.splice(0, 1);
-console.log('My Todos: ', todos);
+// todos.splice(0, 1);
+// console.log('My Todos: ', todos);
+
+// Version 2 Functions
+
+// function displayTodos() {
+//   console.log('My Todos: ', todos);
+// }
+// displayTodos();
+
+// It should have a function to add new todos
+// customise the function
+// function addTodo(todo) {
+//   todos.push(todo);
+//   displayTodos();
+// }
+
+// addTodo('new argument');
+
+// It should have a function to change a todo
+
+// function changeTodo(position, newValue) {
+//   todos[position] = newValue;
+//   displayTodos();
+// }
+
+// changeTodo(0, 'changed');
+
+// It should have a function to delete a todo
+
+// function deleteTodo(position) {
+//   todos.splice(position, 1);
+//   displayTodos();
+// }
+
+// deleteTodo(3);
